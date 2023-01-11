75438e1dd9e3edc0b6f00f54d56398ca74b1c8f8
Apple Man <dan.hassan0910@gmail.com>
2023-01-11 17:34:06 +0900

    prepare table ui

--- src/containers/Scales/index.tsx
+++ src/containers/Scales/index.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 // import components
 import { UploadComponent } from '../../components';
 import { ScalesDisplayComponent } from '../../components';
 
 import Scale from '../../models/Scale'
+
 
 const scales: Scale[] = [
     {
@@ -52,6 +61,26 @@ const categories: string[] = [
     "IEO"
 ]
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
 const ScalesContainer = () => {
     const [stateMsg, setStateMsg] = useState<string>("")
     // const [categories, setCategories] = useState<string[]>([]);
@@ -61,10 +90,35 @@ const ScalesContainer = () => {
     useEffect(() => { }, [])
 
     return (
-        <div>
+        <>
             <UploadComponent />
-            <ScalesDisplayComponent scales={scales} categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-        </div>
+            {/* <ScalesDisplayComponent scales={scales} categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} /> */}
+
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 700 }} aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Date</StyledTableCell>
+                            <StyledTableCell align="right">ProductId</StyledTableCell>
+                            <StyledTableCell align="right">Weight</StyledTableCell>
+                            <StyledTableCell align="right">Unit</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {scales.map((row) => (
+                            <StyledTableRow>
+                                <StyledTableCell component="th" scope="row">
+                                    {row.date}
+                                </StyledTableCell>
+                                <StyledTableCell align="right">{row.productId}</StyledTableCell>
+                                <StyledTableCell align="right">{row.weight.toString()}</StyledTableCell>
+                                <StyledTableCell align="right">{row.unit}</StyledTableCell>
+                            </StyledTableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </>
     )
 }
 
